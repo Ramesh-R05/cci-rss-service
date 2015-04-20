6676e8341db47c3724a50a7f0fa62d52f95b9c64
Bryan Signey <bsigney@dius.com.au>
2015-04-20 17:02:43 +1000

    image resizing and retrieving homepage metadata

--- tests/lib/builders/rssBuilderSpec.js
+++ tests/lib/builders/rssBuilderSpec.js
@@ -2,6 +2,16 @@ var builder = require('../../../lib/builders/rssBuilder');
 var shared = require('mocha-shared');
 
 describe('rssBuilder', function() {
+    var defaultChannelData = {
+        pageTitle_t: "Recipes, Celebrity News, Diet, Living, Family, Food Recipes : Australian Women's Weekly",
+        siteUrl_t: 'http://www.aww.com.au/',
+        pageMetaDescription_t: "Food Recipes, Latest Recipe Collections, Celebrity News, Diet, Living, Family, Parenting, Relationships, Style - Australian Women’s Weekly."
+    };
+    var defaultItemData = {
+        pageTitle_t: 'Item Title',
+        pageMetaDescription_t: 'Item Short Teaser Summary',
+        contentImageUrl_t: 'http://what.image.com'
+    }
 
     shared.behaviour('should have the correct rss feed fields', function(actualFeedOptions) {
         var expectedResult = {
@@ -34,85 +44,77 @@ describe('rssBuilder', function() {
     });
 
     describe('buildShortRssFeed', function() {
+        var data = [
+            defaultChannelData,
+            [defaultItemData]
+        ];
 
-        describe('when there are no items for the feed', function() {
-            var actualFeed = builder.buildShortRssFeed();
-
+        describe('the channel data', function() {
+            var actualFeed = builder.buildShortRssFeed(data);
             shared.behaviour('should have the correct rss feed fields', actualFeed);
-
-            it('should have a new RSS feed without any items', function() {
-                expect(actualFeed.items.length).to.equal(0);
-            });
-
         });
 
-        describe('when there are items for the feed', function() {
-            var items = [
-                {
-                    contentTitle_t: 'Item Title',
-                    contentSummary_t: 'Item Short Teaser Summary',
-                    contentImageUrl_t: 'http://what.image.com'
-                }
-            ];
-
-            var actualFeed = builder.buildShortRssFeed('aww', items);
-
-            shared.behaviour('should have the correct rss feed fields', actualFeed);
-
-            it('should have a new RSS feed with an item', function() {
-                expect(actualFeed.items.length).to.equal(items.length);
-            });
-
-            it('should have item title set to: ' + items[0].contentTitle_t, function() {
-                expect(actualFeed.items[0].title).to.be.equal(items[0].contentTitle_t);
-            });
-
-            it('should have item description set to: ' + items[0].contentSummary_t, function() {
-                expect(actualFeed.items[0].description).to.be.equal(items[0].contentSummary_t);
-            });
-
-            it('should have item enclosure url set to: ' + items[0].contentImageUrl_t, function() {
-                expect(actualFeed.items[0].enclosure.url).to.be.equal(items[0].contentImageUrl_t);
-            });
-
-            describe('and when item has no content summary', function() {
+        describe('the items data', function() {
+            describe('when there are no items for the feed', function() {
 
                 before(function() {
-                   items[0].contentSummary_t = null;
+                    data[1] = null;
                 });
 
                 after(function() {
-                    items[0].contentSummary_t = 'Item Short Teaser Summary';
+                    data[1] = [defaultItemData]
                 });
 
-                it('should have item description set to item title', function() {
-                    var feed = builder.buildShortRssFeed('aww', items);
-                    expect(feed.items[0].description).to.be.equal(items[0].contentTitle_t);
+                var actualFeed = builder.buildShortRssFeed(data);
+                shared.behaviour('should have the correct rss feed fields', actualFeed);
+
+                it('should have a new RSS feed without any items', function() {
+                    actualFeed = builder.buildShortRssFeed(data);
+                    expect(actualFeed.items.length).to.equal(0);
                 });
+
             });
 
-            describe('and when item has no image url', function() {
-                before(function() {
-                    items[0].contentImageUrl_t = null;
+            describe('when there are items for the feed', function() {
+                var actualFeed = builder.buildShortRssFeed(data);
+                var expectedItems = data[1];
+
+                shared.behaviour('should have the correct rss feed fields', actualFeed);
+
+                it('should have a new RSS feed with an item', function() {
+                    expect(actualFeed.items.length).to.equal(expectedItems.length);
                 });
 
-                after(function() {
-                    items[0]. contentImageUrl_t = 'http://what.image.com'
+                it('should have item title set to: ' + expectedItems[0].pageTitle_t, function() {
+                    expect(actualFeed.items[0].title).to.be.equal(expectedItems[0].pageTitle_t);
                 });
 
-                it ('should have no item enclosure', function() {
-                    var feed = builder.buildShortRssFeed('aww', items);
-                    expect(feed.items[0].enclosure).to.be.falsy;
+                it('should have item description set to: ' + expectedItems[0].pageMetaDescription_t, function() {
+                    expect(actualFeed.items[0].description).to.be.equal(expectedItems[0].pageMetaDescription_t);
                 });
-            })
 
+                it('should have item enclosure url set to: ' + expectedItems[0].contentImageUrl_t, function() {
+                    expect(actualFeed.items[0].enclosure.url).to.be.equal(expectedItems[0].contentImageUrl_t + '?width=800');
+                });
+
+                describe('and when item has no image url', function() {
+                    before(function() {
+                        expectedItems[0].contentImageUrl_t = null;
+                    });
+
+                    after(function() {
+                        expectedItems[0]. contentImageUrl_t = 'http://what.image.com'
+                    });
+
+                    it ('should have no item enclosure', function() {
+                        var feed = builder.buildShortRssFeed(data);
+                        expect(feed.items[0].enclosure).to.be.falsy;
+                    });
+                })
+
+            });
         });
-    });
 
-    describe('getSiteFeedOptions', function() {
-        var feedOption = builder.getSiteFeedOptions('aww');
-
-        shared.behaviour('should have the correct rss feed fields', feedOption);
 
     });
 
--- tests/lib/helpers/solrHelperSpec.js
+++ tests/lib/helpers/solrHelperSpec.js
@@ -4,8 +4,8 @@ var nock = require('nock');
 var proxyquire = require('proxyquire');
 
 var urlHelperStub = {
-    getSolrCoreFromRequest: function() {
-        return 'aww-search';
+    getSiteNameFromRequest: function() {
+        return 'aww';
     }
 };
 
@@ -16,86 +16,156 @@ var solrHelper = proxyquire('../../../lib/helpers/solrHelper', {
 describe('solrHelper', function() {
 
     describe('getSearchItems', function() {
+        var solrItemsResult = {
+            responseHeader: {
+                status: 0,
+                QTime: 2
+            },
+            response: {
+                numFound: 1,
+                start: 0,
+                docs: 'hello world!'
+            }
+        };
 
-        describe('Successful response from Solr with valid items', function() {
-            var solrResult = {
-                responseHeader: {
-                    status: 0,
-                    QTime: 2
-                },
-                response: {
-                    numFound: 1,
-                    start: 0,
-                    docs: 'hello world!'
-                }
-            };
+        describe('when searching for homepage data', function() {
 
             before(function() {
                 nock('http://solr-cluster01.digital.dev.local')
                     .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"BauerArticle"&sort=pageDateCreated_dt+desc&rows=50&wt=json')
-                    .reply(200, JSON.stringify(solrResult));
+                    .reply(200, JSON.stringify(solrItemsResult));
             });
 
-            it('should return items from a deferred promise', function() {
-                var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
-                promise.then(function(data) {
-                    expect(data).to.eql(solrResult.docs);
-                    done();
-                }, function(err) {
-                    assert.ok(false, 'deferred promise should not have error');
-                    done();
+            describe('and received successful response from solr', function() {
+
+                describe('and with valid data', function() {
+
+                    before(function() {
+                        nock('http://solr-cluster01.digital.dev.local')
+                            .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"Homepage"&wt=json')
+                            .reply(200, JSON.stringify(solrItemsResult));
+                    });
+
+                    it('should return successful header data from a deferred proomise', function() {
+                        var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
+                        promise.then(function(data) {
+                            expect(data[0]).to.equal(solrItemsResult.response.docs)
+                        }, function(err) {
+                            assert.ok(false, 'deferred promise for header data should not have error');
+                        }).done();
+                    });
+
                 });
 
+                describe('and with no valid data', function() {
+
+                    before(function() {
+                        solrItemsResult.response = {
+                            numFound: 0,
+                            start: 0,
+                            docs: []
+                        };
+
+                        nock('http://solr-cluster01.digital.dev.local')
+                            .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"Homepage"&wt=json')
+                            .reply(200, JSON.stringify(solrItemsResult));
+                    });
+
+                    it('should return error header data from a deferred promise', function() {
+                        var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
+                        promise.then(function(data) {
+                            assert.ok(false, 'deferred promise for header data should not have data');
+                        }, function(err) {
+                            expect(err).to.not.be.null;
+                        }).done();
+                    });
+                });
+            });
+
+            describe('and received unsuccesful response from solr', function() {
+
+                before(function() {
+                    nock('http://solr-cluster01.digital.dev.local')
+                        .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"Homepage"&wt=json')
+                        .reply(400);
+                });
+
+                it('should return error header data from a deferred promise', function() {
+                    var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
+                    promise.then(function(data) {
+                        assert.ok(false, 'deferred promise for header data should not have data');
+                    }, function(err) {
+                        expect(err).to.not.be.null;
+                    }).done();
+                });
             });
         });
 
-        describe('Successful response from Solr with no items', function() {
-            var solrResult = {
-                responseHeader: {
-                    status: 0,
-                    QTime: 2
-                },
-                response: {
-                    numFound: 0,
-                    start: 0,
-                    docs: []
-                }
-            };
+        describe('when searching for items data', function() {
 
             before(function() {
                 nock('http://solr-cluster01.digital.dev.local')
-                    .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"BauerArticle"&sort=pageDateCreated_dt+desc&rows=50&wt=json')
-                    .reply(200, JSON.stringify(solrResult));
+                    .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3AHomepage&wt=json')
+                    .reply(200, JSON.stringify(solrItemsResult));
             });
 
-            it('should return items from a deferred promise', function() {
-                var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
-                promise.then(function(data) {
-                    expect(data).to.be.null;
-                    done();
-                }, function(err) {
-                    assert.ok(false, 'deferred promise should not have error');
-                    done();
+            describe('and received successful response from solr', function() {
+
+                describe('with valid items', function() {
+
+                    before(function() {
+                        nock('http://solr-cluster01.digital.dev.local')
+                            .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"Homepage"&wt=json')
+                            .reply(200, JSON.stringify(solrItemsResult));
+                    });
+
+                    //shared.behaviour('return successful result items from a deferred promise', solrItemsResult.response.docs);
+                    it('should return successful items data from a deferred proomise', function() {
+                        var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
+                        promise.then(function(data) {
+                            expect(data[1]).to.equal(solrItemsResult.response.docs)
+                        }, function(err) {
+                            assert.ok(false, 'deferred promise for header data should not have error');
+                        }).done();
+                    });
                 });
 
-            });
-        });
+                describe('with no items', function() {
 
-        describe('Unsuccesful response from Solr', function() {
-            before(function() {
-                nock('http://solr-cluster01.digital.dev.local')
-                    .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"BauerArticle"&sort=pageDateCreated_dt+desc&rows=50&wt=json')
-                    .reply(400);
+                    before(function() {
+                        solrItemsResult.response.numFound = 0;
+                        solrItemsResult.response.docs = [];
+
+                        nock('http://solr-cluster01.digital.dev.local')
+                            .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"BauerArticle"&sort=pageDateCreated_dt+desc&rows=50&wt=json')
+                            .reply(200, JSON.stringify(solrItemsResult));
+                    });
+
+                    it('should return successful items data from a deferred proomise', function() {
+                        var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
+                        promise.then(function(data) {
+                            expect(data[1]).to.be.null;
+                        }, function(err) {
+                            assert.ok(false, 'deferred promise for header data should not have error');
+                        }).done();
+                    });
+                });
             });
 
-            it ('should return an error', function() {
-                var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
-                promise.then(function(data) {
-                    assert.ok(false, 'deferred promise should not have data');
-                    done();
-                }, function(err) {
-                    expect(err).to.not.be.null;
-                    done();
+            describe('and received unsuccesful response from Solr', function() {
+                before(function() {
+                    nock('http://solr-cluster01.digital.dev.local')
+                        .get('/solr/aww-search/select?q=*%3A*&fq=nodeTypeAlias_t%3A"BauerArticle"&sort=pageDateCreated_dt+desc&rows=50&wt=json')
+                        .reply(400);
+                });
+
+                it('should return error items data from a deferred promise', function() {
+                    var promise = solrHelper.getSearchItems({originalUrl: '/rss/aww'});
+                    promise.then(function(data) {
+                        assert.ok(false, 'deferred promise for items data should not have data');
+                    }, function(err) {
+                        expect(err).to.not.be.null;
+                    }).done();
                 });
             });
         });
