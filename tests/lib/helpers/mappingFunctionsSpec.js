21b5ee99f1da632bbe156fca27afac2572723365
thughes <thughes@bauer-media.com.au>
2015-05-12 09:43:02 +1000

    Unit tests.

--- tests/lib/helpers/mappingFunctionsSpec.js
+++ tests/lib/helpers/mappingFunctionsSpec.js
@@ -159,4 +159,132 @@ describe('mappingFunctions', function () {
 
     });
 
+    describe('mapFullContent', function () {
+
+        var contentUrl = 'http://www.example.com/test-content';
+        var input = [
+            {
+                type: "paragraph",
+                content: "Test paragraph text"
+            },
+            {
+                type: "quote",
+                content: "Test quote text"
+            },
+            {
+                type: "heading",
+                content: "Test heading text"
+            },
+            {
+                type: 'image',
+                content: {
+                    url: "http://www.example.com/test.jpg",
+                    caption: 'Test caption text'
+                }
+            },
+            {
+                type: "video",
+                content: {
+                    properties: {
+                        videoConfiguration: {
+                            brightcoveId: "12345678"
+                        }
+                    }
+                }
+            }
+        ];
+
+        describe('when: valid content set', function () {
+
+            var expectedParagraphText;
+            var expectedQuoteText;
+            var expectedHeadingText;
+            var expectedImageText;
+            var expectedVideoText;
+
+            before(function () {
+                expectedParagraphText = '<p>' + input[0].content + '</p>';
+                expectedQuoteText = '<p>' + input[1].content + '</p>';
+                expectedHeadingText = '<h2>' + input[2].content + '</h2>';
+                expectedImageText = '<img src="' + input[3].content.url + '?width=800" alt="' + input[3].content.caption + '" />';
+                expectedVideoText = '<a href="' + contentUrl + '" target="_blank">Watch video</a>';
+            });
+
+            it('should return formatted content html', function () {
+                var actual = mappingFunctions.mapFullContent(contentUrl, JSON.stringify(input));
+                expect(actual).to.have.string(expectedParagraphText);
+                expect(actual).to.have.string(expectedQuoteText);
+                expect(actual).to.have.string(expectedHeadingText);
+                expect(actual).to.have.string(expectedImageText);
+                expect(actual).to.have.string(expectedVideoText);
+            });
+
+            describe('and when: item content not set', function () {
+
+                var originalVal = input[2].content;
+
+                before(function () {
+                    input[2].content = '';
+                });
+
+                after(function () {
+                    input[2].content = originalVal;
+                })
+
+                it('should ignore the item', function () {
+                    var actual = mappingFunctions.mapFullContent(contentUrl, JSON.stringify(input));
+                    expect(actual).to.not.have.string('<h2>');
+                })
+            });
+
+            describe('and when: item video id not set', function () {
+
+                var originalVal = input[4].content.properties.videoConfiguration.brightcoveId;
+
+                before(function () {
+                    input[4].content.properties.videoConfiguration.brightcoveId = '';
+                });
+
+                after(function () {
+                    input[4].content.properties.videoConfiguration.brightcoveId = originalVal;
+                });
+
+                it('should not include a video link', function () {
+                    var actual = mappingFunctions.mapFullContent(contentUrl, JSON.stringify(input));
+                    expect(actual).to.not.have.string('Watch video');
+                });
+            });
+
+            describe('and when: image settings passed as parameter', function () {
+
+                var settings = {
+                    image: {
+                        width: 640,
+                        height: 480
+                    }
+                };
+
+                before(function () {
+                    expectedImageText = '<img src="' + input[3].content.url + '?width=' + settings.image.width + '&height=' + settings.image.height + '" alt="' + input[3].content.caption + '" />';
+                });
+
+                it('should use the image setting values', function () {
+                    var actual = mappingFunctions.mapFullContent(contentUrl, JSON.stringify(input), JSON.stringify(settings));
+                    expect(actual).to.have.string(expectedImageText);
+                });
+            });
+
+        });
+
+        describe('when: invalid content set', function () {
+
+            it('should return an empty string', function () {
+                var actual = mappingFunctions.mapFullContent(contentUrl, 'BAD JSON');
+                expect(actual).to.be.empty;
+            });
+
+        });
+
+    });
+
 });
---
+++ tests/lib/helpers/markdownHelperSpec.js
@@ -0,0 +1,117 @@
+﻿var markdownHelper = require('../../../lib/helpers/markdownHelper');
+
+describe('markdownHelper', function () {
+
+    describe('renderParagraph', function () {
+
+        var input;
+        var expectedOutput;
+
+        describe('when: content set', function () {
+
+            before(function () {
+                input = '**Hello World**'
+                expectedOutput = "<p><strong>Hello World</strong></p>\n";
+            });
+
+            it('should return a paragraph containing html markup', function () {
+                var actual = markdownHelper.renderParagraph(input);
+                expect(actual).to.equal(expectedOutput);
+            });
+
+        });
+
+        describe('when: content not set', function () {
+
+            before(function () {
+                input = null;
+            });
+
+            it('should return an empty string', function () {
+                var actual = markdownHelper.renderParagraph(input);
+                expect(actual).to.be.empty;
+            });
+
+        });
+
+    });
+
+    describe('renderImage', function () {
+
+        var input = {
+            url: 'http://www.example.com/test.jpg',
+            caption: ''
+        };
+
+        describe('when: content set', function () {
+
+            var expectedImageHtml;
+
+            before(function () {
+                expectedImageHtml = '<img src="' + input.url + '" alt="' + input.caption + '" />';
+            });
+
+            it('should return html containing an image', function () {
+                var actual = markdownHelper.renderImage(input);
+                expect(actual).to.have.string(expectedImageHtml);
+            });
+
+            describe('and when: image attributes are set', function () {
+
+                var attributes = {
+                    width: 800,
+                    height: 600
+                };
+
+                before(function () {
+                    expectedImageHtml = '<img src="' + input.url + '?width=' + attributes.width + '&height=' + attributes.height + '" alt="' + input.caption + '" />';
+                });
+
+                it('should append attributes as query string parameters to the image url', function () {
+                    var actual = markdownHelper.renderImage(input, attributes);
+                    expect(actual).to.have.string(expectedImageHtml);
+                });
+
+            });
+
+            describe('and when: image caption is set', function () {
+
+                var expectedCaptionHtml;
+
+                before(function () {
+                    input.caption = '**Some caption**';
+                    expectedImageHtml = '<img src="' + input.url + '" alt="Some caption" />';
+                    expectedCaptionHtml = '<p><strong>Some caption</strong></p>';
+                });
+
+                it('should return html with a paragraph containing the formatted caption text', function () {
+                    var actual = markdownHelper.renderImage(input);
+                    expect(actual).to.have.string(expectedImageHtml);
+                    expect(actual).to.have.string(expectedCaptionHtml);
+                });
+
+            });
+
+        });
+
+        describe('when: content not set', function () {
+
+            var originalVal = input.url;
+
+            before(function () {
+                input.url = '';
+            });
+
+            after(function () {
+                input.url = originalVal;
+            });
+
+            it('should return an empty string', function () {
+                var actual = markdownHelper.renderImage(input);
+                expect(actual).to.be.empty;
+            });
+        })
+
+    });
+
+});
