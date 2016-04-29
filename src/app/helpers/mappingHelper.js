2866899f69a90bebc0db793b07d55528edeaa0a1
Min Park <mpark@bauerxcelmedia.com.au>
2016-04-29 11:55:29 +1000

    lint

--- src/app/helpers/mappingHelper.js
+++ src/app/helpers/mappingHelper.js
@@ -1,5 +1,122 @@
 ﻿import utils from'../utils';
 
+let mapValue = (config, mapData, defaultValue) => {
+    if (typeof config === 'string') {
+        return utils.getProperty(config, mapData, defaultValue);
+    } else if (utils.isFunctionConfig(config)) {
+        let fn = utils.compileFunction(config, mapData);
+        return fn.execute();
+    }
+    return defaultValue;
+};
+
+let map = (config, mapData, sourceValue, postMapFunctions) => {
+    let val = '';
+    let defaultValue = sourceValue;
+
+    if (!defaultValue) {
+        defaultValue = '';
+    }
+
+    if (config && config instanceof Array) {
+        for (let i = 0; i < config.length; i++) {
+            let mapKey = config[i];
+            val = mapValue(mapKey, mapData, defaultValue);
+            if (val !== defaultValue) {
+                break;
+            }
+        }
+    } else {
+        val = mapValue(config, mapData, defaultValue);
+    }
+
+    if (postMapFunctions) {
+        postMapFunctions.forEach(item => {
+            let fn = utils.compileFunction(item, mapData, [val]);
+            val = fn.execute();
+        });
+    }
+
+    return val;
+};
+
+let mapArray = (config, mapData, defaultValue, postMapFunctions) => {
+    let arr = [];
+
+    if (config && config instanceof Array) {
+        config.forEach(mapKey => {
+            arr.push(map(mapKey, mapData, defaultValue, postMapFunctions));
+        });
+    }
+
+    return arr;
+};
+
+let mapObject = (config, mapData) => {
+    let obj = {};
+
+    for (let i in config) {
+        if (config.hasOwnProperty(i)) {
+            /*eslint-disable */
+            obj[i] = mapDataItemProperty(config[i], mapData);
+            /*eslint-enable */
+        }
+    }
+
+    return obj;
+};
+
+let mapObjectArray = (config, mapData) => {
+    let arr = [];
+    let obj = mapObject(config, mapData);
+
+    for (let i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            let o = {};
+            o[i] = obj[i];
+            arr.push(o);
+        }
+    }
+
+    return arr;
+};
+
+let mapDataItemProperty = (config, mapData) => {
+    if (config && config.value) {
+        return config.value;
+    }
+
+    if (config && config.map) {
+        return map(config.map, mapData, config.default, config.afterMap);
+    }
+
+    if (config && config.mapArray) {
+        return mapArray(config.mapArray, mapData, config.default, config.afterMap);
+    }
+
+    if (config && config.mapObject) {
+        return mapObject(config.mapObject, mapData);
+    }
+
+    if (config && config.mapObjectArray) {
+        return mapObjectArray(config.mapObjectArray, mapData);
+    }
+};
+
+let mapDataItem = (mappingConfigs, mapData) => {
+    let mapped = {};
+
+    mappingConfigs.forEach(mapConfig => {
+        for (let j in mapConfig) {
+            if (mapConfig.hasOwnProperty(j)) {
+                mapped[j] = mapDataItemProperty(mapConfig[j], mapData);
+            }
+        }
+    });
+
+    return mapped;
+};
+
 let mapDataItems = (mappingConfigs, dataItems, additionalMapData) => {
     let mappedItems = [];
 
@@ -7,7 +124,9 @@ let mapDataItems = (mappingConfigs, dataItems, additionalMapData) => {
         dataItems.forEach(mapData => {
             if (additionalMapData) {
                 for (let j in additionalMapData) {
-                    mapData[j] = additionalMapData[j];
+                    if (additionalMapData.hasOwnProperty(j)) {
+                        mapData[j] = additionalMapData[j];
+                    }
                 }
             }
             mappedItems.push(mapDataItem(mappingConfigs, mapData));
@@ -50,114 +169,6 @@ let mapSolrData = props => {
     return mapped;
 };
 
-
-let mapDataItem = (mappingConfigs, mapData) => {
-    let mapped = {};
-
-    mappingConfigs.forEach(mapConfig => {
-        for (let j in mapConfig) {
-            mapped[j] = mapDataItemProperty(mapConfig[j], mapData);
-        }
-    });
-
-    return mapped;
-};
-
-let mapDataItemProperty = (config, mapData) => {
-    if (config && config.value) {
-        return config.value;
-    }
-
-    if (config && config.map) {
-        return map(config.map, mapData, config.default, config.afterMap);
-    }
-
-    if (config && config.mapArray) {
-        return mapArray(config.mapArray, mapData, config.default, config.afterMap);
-    }
-
-    if(config && config.mapObject) {
-        return mapObject(config.mapObject, mapData);
-    }
-
-    if (config && config.mapObjectArray) {
-        return mapObjectArray(config.mapObjectArray, mapData);
-    }
-};
-
-let map = (config, mapData, defaultValue, postMapFunctions) => {
-    let val = '';
-
-    if (!defaultValue) {
-        defaultValue = '';
-    }
-
-    if (config && config instanceof Array) {
-        for(let i = 0; i < config.length; i++){
-            let mapKey = config[i];
-            val = mapValue(mapKey, mapData, defaultValue);
-            if(val !== defaultValue){
-                break;
-            }
-        }
-    } else {
-        val = mapValue(config, mapData, defaultValue);
-    }
-
-    if (postMapFunctions) {
-        postMapFunctions.forEach(item => {
-            let fn = utils.compileFunction(item, mapData, [val]);
-            val = fn.execute();
-        });
-    }
-
-    return val;
-};
-
-let mapArray = (config, mapData, defaultValue, postMapFunctions) => {
-    let arr = [];
-
-    if (config && config instanceof Array) {
-        config.forEach(mapKey => {
-            arr.push(map(mapKey, mapData, defaultValue, postMapFunctions));
-        });
-    }
-
-    return arr;
-};
-
-let mapObject = (config, mapData) => {
-    let obj = {};
-
-    for (let i in config) {
-        obj[i] = mapDataItemProperty(config[i], mapData);
-    }
-
-    return obj;
-};
-
-let mapObjectArray = (config, mapData) => {
-    let arr = [];
-    let obj = mapObject(config, mapData);
-
-    for (let i in obj) {
-        let o = {};
-        o[i] = obj[i];
-        arr.push(o);
-    }
-
-    return arr;
-};
-
-let mapValue = (config, mapData, defaultValue) => {
-    if (typeof config === 'string') {
-        return utils.getProperty(config, mapData, defaultValue);
-    } else if (utils.isFunctionConfig(config)) {
-        let fn = utils.compileFunction(config, mapData);
-        return fn.execute();
-    }
-    return defaultValue;
-};
 
 export default {
     mapSolrData,
