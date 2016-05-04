e5a93d2717de656553f989f2b2f3f962d5ec5181
Min Park <mpark@bauerxcelmedia.com.au>
2016-05-04 11:37:05 +1000

    get image length

--- src/app/helpers/rssHelper.js
+++ src/app/helpers/rssHelper.js
@@ -1,10 +1,14 @@
 ﻿'use strict';
 
+var async = require('async');
+var request = require('request');
+var HttpProxyAgent = require('http-proxy-agent');
+var Q = require('q');
+var _ = require('lodash');
 var solrHelper = require('./solrHelper');
 var mapper = require('./mappingHelper');
 var builder = require('../builders/rssBuilder');
 var utils = require('../utils');
-var Q = require('q');
 
 //HACK: To test DDO-406
 //TODO: If works, deal with it in mappings.json. If not, remove this
@@ -20,8 +24,39 @@ var dollyTest = function (site, sourceFeed) {
     });
 }
 
-var buildFeed = function (props, site) {
+var agent = process.env.NODE_ENV
+    ? null
+    : new HttpProxyAgent('http://sydproxy.acp.net:8080');   //Has proxy issue in local
+var setImageLength = function(items) {
+    var deferred = Q.defer();
 
+    async.map(items, function(item, callback) {
+        //HACK: Ignores tests with 'example.com'
+        if (!item.enclosure || !item.enclosure.url || _.includes(item.enclosure.url, 'example.com')) {
+            callback(null, null);
+        } else {
+            request({ uri: item.enclosure.url, method: "GET", agent: agent }, function (error, response) {
+                if (!error && response.statusCode == 200) {
+                    item.enclosure.size = response.headers['content-length'];
+                    callback(null, null);
+                } else {
+                    callback(error || response.statusCode);
+                }
+            });
+        }
+    }, function(err) {
+        if (!err) {
+            deferred.resolve();
+        } else {
+            console.error('[ERROR] ' + err);
+            deferred.reject(err);
+        }
+    });
+
+    return deferred.promise;
+}
+
+var buildFeed = function (props, site) {
     var deferred = Q.defer();
 
     solrHelper.loadData(props)
@@ -31,7 +66,12 @@ var buildFeed = function (props, site) {
             try {
                 var feed = builder.buildFeed(mapper.mapSolrData(props));
                 dollyTest(site, feed);
-                deferred.resolve(feed.xml());
+                setImageLength(feed.items).then(function() {
+                    deferred.resolve(feed.xml());
+                }, function (err) {
+                    console.error('[ERROR] ' + err);
+                    res.sendStatus(500);
+                });
             }
             catch (err) {
                 deferred.reject(err.message);
