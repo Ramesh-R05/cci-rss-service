58fddceb49857d815840c8c7f535d8102235a0f4
Min Park <mpark@bauerxcelmedia.com.au>
2016-05-06 14:43:06 +1000

    lint

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,6 +1,18 @@
 ﻿import stringHelper from './helpers/stringHelper';
 import functions from './functions';
 
+function isFunction(obj) {
+    return (obj && typeof obj === 'function');
+}
+
+function isFunctionConfig(obj) {
+    return (obj && obj.fn && typeof obj.fn === 'string');
+}
+
+function isArray(obj) {
+    return (obj && obj instanceof Array);
+}
+
 function getProperty(propName, obj, defaultValue) {
     try {
         let parts = stringHelper.split(propName, '.', true);
@@ -18,65 +30,6 @@ function getProperty(propName, obj, defaultValue) {
     }
 
     return defaultValue;
-}
-
-function compileFunction(config, data, additionalParams) {
-    let fn = {
-        func: function() {
-            return '';
-        },
-        params: [],
-        scope: this,
-        execute: function() {
-            return this.func.apply(this.scope, this.params);
-        }
-    };
-
-    if (config && config.fn) {
-        let fnInfo = getFunctionInfo(config);
-        if (fnInfo) {
-            fn.func = fnInfo.func;
-            fn.scope = fnInfo.scope;
-            if (config.params) {
-                config.params.forEach(param => {
-                    fn.params.push(compileFunctionParameter(param, data));
-                });
-            }
-            if (isArray(additionalParams)) {
-                fn.params = fn.params.concat(additionalParams);
-            }
-        }
-    }
-    return fn;
-}
-
-function compileFunctionParameter(param, data) {
-    if (typeof param === 'string') {
-        return bindParameterValue(param, data);
-    }
-
-    if (isFunctionConfig(param)) {
-        return compileFunction(param, data).execute();
-    }
-
-    if (param && typeof param === 'object') {
-        let obj = isArray(param) ? [] : {};
-        for (let i in param) {
-            if (param.hasOwnProperty(i)) {
-                obj[i] = compileFunctionParameter(param[i], data);
-            }
-        }
-        return obj;
-    }
-    return param;
-}
-
-function bindParameterValue(bindingKey, bindingData) {
-    let val = bindingKey;
-    if (bindingKey.length > 1 && bindingKey.indexOf('@') === 0) {
-        val = getProperty(bindingKey.substring(1), bindingData, '');
-    }
-    return val;
 }
 
 function getFunctionInfo(config) {
@@ -116,16 +69,65 @@ function getFunctionInfo(config) {
     return null;
 }
 
-function isFunction(obj) {
-    return (obj && typeof obj === 'function');
+function bindParameterValue(bindingKey, bindingData) {
+    let val = bindingKey;
+    if (bindingKey.length > 1 && bindingKey.indexOf('@') === 0) {
+        val = getProperty(bindingKey.substring(1), bindingData, '');
+    }
+    return val;
 }
 
-function isFunctionConfig(obj) {
-    return (obj && obj.fn && typeof obj.fn === 'string');
+function compileFunctionParameter(param, data) {
+    if (typeof param === 'string') {
+        return bindParameterValue(param, data);
+    }
+
+    if (isFunctionConfig(param)) {
+        /*eslint-disable*/
+        return compileFunction(param, data).execute();
+        /*eslint-enable*/
+    }
+
+    if (param && typeof param === 'object') {
+        let obj = isArray(param) ? [] : {};
+        for (let i in param) {
+            if (param.hasOwnProperty(i)) {
+                obj[i] = compileFunctionParameter(param[i], data);
+            }
+        }
+        return obj;
+    }
+    return param;
 }
 
-function isArray(obj) {
-    return (obj && obj instanceof Array);
+function compileFunction(config, data, additionalParams) {
+    let fn = {
+        func: function() {
+            return '';
+        },
+        params: [],
+        scope: this,
+        execute: function() {
+            return this.func.apply(this.scope, this.params);
+        }
+    };
+
+    if (config && config.fn) {
+        let fnInfo = getFunctionInfo(config);
+        if (fnInfo) {
+            fn.func = fnInfo.func;
+            fn.scope = fnInfo.scope;
+            if (config.params) {
+                config.params.forEach(param => {
+                    fn.params.push(compileFunctionParameter(param, data));
+                });
+            }
+            if (isArray(additionalParams)) {
+                fn.params = fn.params.concat(additionalParams);
+            }
+        }
+    }
+    return fn;
 }
 
 export default {
